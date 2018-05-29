71c61afdc64e84868c366128ab87c7b1c953de2f
Cht <zzshkjb@126.com>
2018-05-29 14:47:18 +0800

    测试提交

---
+++ application/admins/controller/Home.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\admins\controller;
+use think\Controller;
+use Util\data\Sysdb;
+/**
+ *后台首页
+ */
+class Home  extends BaseAdmin
+{
+    public function index(){
+        return $this->fetch();
+    }
+
+    public function welcome(){
+        return $this->fetch();
+    }
+}
